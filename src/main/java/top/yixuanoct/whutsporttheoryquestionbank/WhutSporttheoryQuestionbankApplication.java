547b5cd3793fca93542956acbbc48f748d36980d
YixuanOct <yixuanoct@gmail.com>
2024-10-31 10:40:21 +0800

    Initial commit

---
+++ src/main/java/top/yixuanoct/whutsporttheoryquestionbank/WhutSporttheoryQuestionbankApplication.java
@@ -0,0 +1,13 @@
+package top.yixuanoct.whutsporttheoryquestionbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhutSporttheoryQuestionbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhutSporttheoryQuestionbankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/yixuanoct/whutsporttheoryquestionbank/WhutSporttheoryQuestionbankApplicationTests.java
@@ -0,0 +1,13 @@
+package top.yixuanoct.whutsporttheoryquestionbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhutSporttheoryQuestionbankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
